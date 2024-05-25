0ee7c88c16613885cf6150c5dbdf75ebbf147569
Joao Victor Assis <joaovictorassis03@gmail.com>
2024-05-25 17:11:07 -0300

    Criando conexao com mongoDB usando Atlas para rodar em nuvem e criando primeiro arquivo do servidor express

---
+++ src/models/db.js
@@ -0,0 +1,13 @@
+const mongoose = require ("mongoose")
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect('mongodb+srv://globalfinanceiro:testeDigimon@digimon-api.z5tegwh.mongodb.net/?retryWrites=true&w=majority&appName=digimon-api')
+        console.log("Banco de dados conectado com sucesso!")
+    } catch (error){
+        console.log("Falha ao conectar no banco de dados.", error)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
